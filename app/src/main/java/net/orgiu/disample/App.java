408352629b4491ab7fe1ae550a0e301911066ae3
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-08-08 12:35:43 +0200

    Builds the component

---
+++ app/src/main/java/net/orgiu/disample/App.java
@@ -0,0 +1,20 @@
+package net.orgiu.disample;
+
+
+import android.app.Application;
+
+public class App extends Application{
+
+    private ApplicationComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        component = DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this)).build();
+    }
+
+    public ApplicationComponent getComponent() {
+        return component;
+    }
+}
--- app/src/main/java/net/orgiu/disample/ApplicationComponent.java
+++ app/src/main/java/net/orgiu/disample/ApplicationComponent.java
@@ -1,7 +1,10 @@
 package net.orgiu.disample;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
+@Singleton
 @Component(modules = {ApplicationModule.class})
 public interface ApplicationComponent {
 

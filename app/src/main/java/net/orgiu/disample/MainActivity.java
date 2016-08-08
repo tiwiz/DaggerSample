7cdf417da9174759522e031469863b00607f2e14
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-08-08 12:35:57 +0200

    Makes the first injection

--- app/src/main/java/net/orgiu/disample/MainActivity.java
+++ app/src/main/java/net/orgiu/disample/MainActivity.java
@@ -1,13 +1,22 @@
 package net.orgiu.disample;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((App) getApplication()).getComponent().inject(this);
     }
 }

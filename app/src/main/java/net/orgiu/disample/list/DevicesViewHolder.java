16c42178117c93c94fa2df6fdb8489efc9f3b6df
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-08-17 17:20:17 +0200

    Adds new UI to the main view of the project

--- app/src/main/java/net/orgiu/disample/list/DevicesViewHolder.java
+++ app/src/main/java/net/orgiu/disample/list/DevicesViewHolder.java
@@ -4,20 +4,26 @@ package net.orgiu.disample.list;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import net.orgiu.disample.R;
 import net.orgiu.disample.database.RealmDevice;
 
 class DevicesViewHolder extends RecyclerView.ViewHolder {
+    private final ImageView modelImageView;
     private final TextView modelTextView;
 
     DevicesViewHolder(View itemView) {
         super(itemView);
+        modelImageView = (ImageView) itemView.findViewById(R.id.imgDevice);
         modelTextView = (TextView) itemView.findViewById(R.id.txtModel);
     }
 
     void bindTo(@NonNull RealmDevice device) {
+        Glide.with(modelImageView.getContext()).load(device.getImageUrl()).into(modelImageView);
         modelTextView.setText(DeviceUtils.clearFromBrand(device));
     }
 }

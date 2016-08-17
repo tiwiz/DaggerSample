6029f3040176d7f6e7e62e0067592bf53877b489
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-08-17 22:45:06 +0200

    Updates the support libraries

---
+++ app/src/main/java/net/orgiu/disample/DeviceInfoActivity.java
@@ -0,0 +1,13 @@
+package net.orgiu.disample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_info);
+    }
+}
